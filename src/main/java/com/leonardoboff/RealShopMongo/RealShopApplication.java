68cdf115087770fe043bc987ab7fa84a1db9b464
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-02-02 15:56:28 -0300

    Init remote repository

---
+++ src/main/java/com/leonardoboff/RealShopMongo/RealShopApplication.java
@@ -0,0 +1,13 @@
+package com.leonardoboff.RealShopMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardoboff/RealShopMongo/RealShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoboff.RealShopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
