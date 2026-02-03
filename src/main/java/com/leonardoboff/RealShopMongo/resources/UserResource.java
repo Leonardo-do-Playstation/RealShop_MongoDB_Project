85edd78cdd892017b95e4e57e19994deab1c0153
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-02-03 13:32:13 -0300

    Obtains one user by id

--- src/main/java/com/leonardoboff/RealShopMongo/resources/UserResource.java
+++ src/main/java/com/leonardoboff/RealShopMongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +14,23 @@ import com.leonardoboff.RealShopMongo.domain.User;
 import com.leonardoboff.RealShopMongo.dtos.UserDTO;
 import com.leonardoboff.RealShopMongo.services.UserService;
 
-
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@Autowired
 	UserService userService;
 
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = userService.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 
-    @GetMapping
-    public ResponseEntity<List<UserDTO>> findAll() {
-        List<User> list = userService.findAll();
-        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDto);
-    }
+	@RequestMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
-
-
