2415e88bae90b2947d128af9499fb49c9585a758
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-02-03 18:41:31 -0300

    Endpoint to return posts from an user

--- src/main/java/com/leonardoboff/RealShopMongo/resources/UserResource.java
+++ src/main/java/com/leonardoboff/RealShopMongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.leonardoboff.RealShopMongo.domain.Post;
 import com.leonardoboff.RealShopMongo.domain.User;
 import com.leonardoboff.RealShopMongo.dtos.UserDTO;
 import com.leonardoboff.RealShopMongo.services.UserService;
@@ -60,6 +61,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
