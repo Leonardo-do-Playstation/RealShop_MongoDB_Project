996858d985ca06dbbd55289bb9f97e3aac418da8
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-02-03 17:55:50 -0300

    Create a post entity with a nested user

---
+++ src/main/java/com/leonardoboff/RealShopMongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.leonardoboff.RealShopMongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leonardoboff.RealShopMongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
