e0055b5f861a400af76eb1db2ee8590c538ec3b7
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-02-03 20:49:08 -0300

    Query with many criterias

--- src/main/java/com/leonardoboff/RealShopMongo/services/PostService.java
+++ src/main/java/com/leonardoboff/RealShopMongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.leonardoboff.RealShopMongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,8 +21,13 @@ public class PostService {
 		Optional<Post> user = postRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado! Id = " + id));
 	}
-	 
+
 	public List<Post> findByTitle(String text) {
 		return postRepository.searchTitle(text);
+	}
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 }
