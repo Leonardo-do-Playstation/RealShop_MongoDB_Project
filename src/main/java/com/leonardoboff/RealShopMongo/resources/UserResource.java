49c27bbb6d86f5f8c39349090119983a21d57740
Leonardo Antonio de Oliveira Boff <leooliveiraboff@gmail.com>
2026-02-03 15:27:45 -0300

    Creates insert methods

--- src/main/java/com/leonardoboff/RealShopMongo/resources/UserResource.java
+++ src/main/java/com/leonardoboff/RealShopMongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.leonardoboff.RealShopMongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leonardoboff.RealShopMongo.domain.User;
 import com.leonardoboff.RealShopMongo.dtos.UserDTO;
@@ -28,9 +32,17 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
-	@RequestMapping(value = "/{id}")
+	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = userService.fromDTO(objDto);
+		obj = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/leonardoboff/RealShopMongo/services/UserService.java
+++ src/main/java/com/leonardoboff/RealShopMongo/services/UserService.java
@@ -7,21 +7,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leonardoboff.RealShopMongo.domain.User;
+import com.leonardoboff.RealShopMongo.dtos.UserDTO;
 import com.leonardoboff.RealShopMongo.repository.UserRepository;
 import com.leonardoboff.RealShopMongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado! Id = " + id));
 	}
+
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
